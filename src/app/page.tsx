00e96be02c1178f7e9a7087a3b1bfb4edf123388
Andriikryl <andreyk557@gmail.com>
2023-11-03 22:04:48 +0200

    add layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,31 @@ import clsx from "clsx";
 
 export default function Home() {
   return (
-    <>
-      <div>asdad</div>
-    </>
+    <section className={styles.blog}>
+      <Container>
+        <div className={styles.blog__box}>
+          <div className={styles.blog__sidebar}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto
+            itaque recusandae aut, similique cupiditate earum hic optio deserunt
+            quis quam sed et eum, corrupti dolor blanditiis aperiam accusamus
+            magnam non.
+          </div>
+          <div className={styles.blog__content}>
+            <div className={styles.blog__wrapper}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laboriosam sequi culpa atque reiciendis nobis enim eveniet
+                earum. Reiciendis magnam laudantium, deleniti voluptatum
+                similique optio pariatur vel ea, aspernatur ipsum consectetur.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Expedita ad placeat odio excepturi molestias unde totam illo
+                reiciendis deleniti perspiciatis aperiam deserunt in,
+                dignissimos quae beatae ipsum? Nostrum, voluptate fugiat.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
   );
 }
